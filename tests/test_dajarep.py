37d8b921f3cc9b6acca72b1f8ce31536d3bc47a1
kurehajime <x.iidec+gh@googlemail.com>
2015-09-27 20:32:53 +0900

    add test

--- tests/test_dajarep.py
+++ tests/test_dajarep.py
@@ -8,6 +8,7 @@ class TestDajarep(unittest.TestCase):
         self.assertEqual(dajarating.dajarep.dajarep(u"猫が寝転んだ"), u"ネコ")  # ダジャレ
         self.assertEqual(dajarating.dajarep.dajarep(u"布団が吹っ飛んだ"), u"フトン")  # ダジャレ
         self.assertEqual(dajarating.dajarep.dajarep(u"イカは如何なものか"), u"イカ")  # ダジャレ
+        self.assertEqual(dajarating.dajarep.dajarep(u"智代子のチョコ"), u"チョコ")  # ダジャレ
         self.assertEqual(dajarating.dajarep.dajarep(u"今日は良い天気ですね"), u"")  # NOTダジャレ
         self.assertEqual(dajarating.dajarep.dajarep(u"犬が犬小屋にいる"), u"")  # NOTダジャレ
         self.assertEqual(dajarating.dajarep.dajarep(u"人民の人民による人民のための政治"), u"")  # NOTダジャレ
